6c605db3fc525f59559727423897c52726b229e4
changfeng777 <863241056@qq.com>
2015-09-14 12:22:57 +0800

    xjh：提交临接表的存储实现。

--- Graph/Graph/Graph.hpp
+++ Graph/Graph/Graph.hpp
@@ -8,12 +8,10 @@ class GraphMatrix
 {
 public:
 	GraphMatrix()
-		:_edgeSize(0)
 	{}
 
 	GraphMatrix(const V* vertexs, int size)
-		:_edgeSize(0)
-		,_vertexSize(size)
+		:_vertexSize(size)
 	{
 		// 开辟矩阵和边集
 		_matrix = new W*[_vertexSize];
@@ -78,7 +76,7 @@ private:
 	W**	 _matrix;		// 临接矩阵
 	V*	 _vertexs;		// 顶点集			
 	int	 _vertexSize;	// 顶点数
-	int	 _edgeSize;		// 边条数
+	//int	 _edgeSize;		// 边条数
 };
 
 // 无向图
@@ -99,6 +97,145 @@ void Test1()
 void Test2()
 {
 	GraphMatrix<char, int> g("ABCDE", 5);
+	g.AddEdge('A', 'D', 10);
+	g.AddEdge('E', 'A', 20);
+	g.AddEdge('B', 'C', 10);
+	g.AddEdge('D', 'B', 20);
+	g.AddEdge('E', 'B', 30);
+	g.AddEdge('C', 'E', 40);
+
+	g.Display();
+}
+
+
+template<class V, class W>
+struct LinkEdge
+{
+	int _index;				// 顶点下标
+	W _weight;				// 权重
+	LinkEdge<V, W>* _next;	// 指向下一个节点的指针 
+
+	LinkEdge(int index = -1, const W& weight = W())
+		:_index(index)
+		,_weight(weight)
+		,_next(NULL)
+	{}
+};
+
+template<class V, class W>
+struct LinkVertex
+{
+	V _vertex;				// 顶点
+	LinkEdge<V, W>* _head;	// 指向临接表的边
+
+	LinkVertex(const V& vertex = V())
+		:_vertex(vertex)
+		,_head(NULL)
+	{}
+};
+
+template<class V, class W>
+class GraphLink
+{
+public:
+	GraphLink()
+		:_linkTable(0)
+		,_vertexSize(0)
+	{}
+
+	GraphLink(const V* ar, int size)
+		:_vertexSize(size)
+	{
+		_linkTable = new LinkVertex<V, W>[size];
+		for (int i = 0; i < size; ++i)
+		{
+			_linkTable[i]._vertex = ar[i];
+		}
+	}
+
+	~GraphLink()
+	{}
+
+public:
+	int GetVertexIndex(const V& vertex)
+	{
+		for (int i = 0; i < _vertexSize; ++i)
+		{
+			if(_linkTable[i]._vertex == vertex)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	void _AddEdge(int srcIndex, int dstIndex, const W& weight)
+	{
+		LinkEdge<V, W>* head = _linkTable[srcIndex]._head;
+		LinkEdge<V, W>* tmp = new LinkEdge<V, W>(dstIndex, weight);
+
+		tmp->_next = head;
+		_linkTable[srcIndex]._head = tmp;
+	}
+
+	void AddEdge(const V& src, const V& dst, const W& weight)
+	{
+		int srcIndex = GetVertexIndex(src);
+		int dstIndex = GetVertexIndex(dst);
+
+		assert(srcIndex != -1);
+		assert(dstIndex != -1);
+
+		// 无向图
+		_AddEdge(srcIndex, dstIndex, weight);
+		_AddEdge(dstIndex, srcIndex, weight);
+
+		// 有向图
+		//_AddEdge(srcIndex, dstIndex, weight);
+	}
+
+	void Display()
+	{
+		for (int i = 0; i < _vertexSize; ++i)
+		{
+			cout<<_linkTable[i]._vertex<<"["<<i<<"]->";
+			LinkEdge<V, W>* begin = _linkTable[i]._head;
+			while (begin)
+			{
+				cout<<begin->_weight<<"["<<begin->_index<<"]""->";
+				begin = begin->_next;
+			}
+
+			cout<<"NULL"<<endl;
+		}
+
+		cout<<endl;
+	}
+
+protected:
+	LinkVertex<V, W>* _linkTable;	// 临接表
+	int _vertexSize;				// 顶点个数
+};
+
+// 无向图
+void Test3()
+{
+	GraphLink<char, int> g("ABCDE", 5);
+	g.AddEdge('A', 'D', 10);
+	g.AddEdge('A', 'E', 20);
+	g.AddEdge('B', 'C', 10);
+	g.AddEdge('B', 'D', 20);
+	g.AddEdge('B', 'E', 30);
+	g.AddEdge('C', 'E', 40);
+
+	g.Display();
+}
+
+// 有向图
+void Test4()
+{
+	GraphLink<char, int> g("ABCDE", 5);
 	g.AddEdge('A', 'D', 10);
 	g.AddEdge('E', 'A', 20);
 	g.AddEdge('B', 'C', 10);
