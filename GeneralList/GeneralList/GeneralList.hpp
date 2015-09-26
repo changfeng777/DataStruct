bdefc46fc603cd9cfd8c43b1e9e4a3268812621c
changfeng777 <863241056@qq.com>
2015-09-26 13:42:12 +0800

    xjh:提交广义表Copy/Size/Depth等递归算法的实现。

--- GeneralList/GeneralList/GeneralList.hpp
+++ GeneralList/GeneralList/GeneralList.hpp
@@ -2,7 +2,7 @@
 
 enum TYPE
 {
-	HEAD_TYPE = 0,		// 附加头节点
+	HEAD_TYPE = 0,		// 附加头节点nm
 	VALUE_TYPE = 1,		// 存储值的节点
 	SUB_TYPE = 2,		// 指向子表
 };
@@ -49,7 +49,20 @@ public:
 	}
 
 	~GeneralList()
-	{}
+	{
+		Destory();
+	}
+
+	GeneralList(const GeneralList& g)
+	{
+		_head = g.Copy();
+	}
+
+	GeneralList& operator=(GeneralList g)
+	{
+		swap(_head, g._head);
+		return *this;
+	}
 
 public:
 	void CreateList(GeneralListNode*& head, const char*& str)
@@ -95,6 +108,28 @@ public:
 		cout<<endl;
 	}
 
+	int Depth()
+	{
+		return _Depth(_head);
+	}
+
+	int Size()
+	{
+		return _Size(_head);
+	}
+
+	void Destory()
+	{
+		_Destroy(_head);
+		_head = NULL;
+	}
+
+	GeneralListNode* Copy() const
+	{
+		return _Copy(_head);
+	}
+
+protected:
 	void _Print(GeneralListNode* head)
 	{
 		GeneralListNode* begin = head;
@@ -130,40 +165,139 @@ public:
 		cout<<")";
 	}
 
-	GeneralListNode* Find(char value)
-	{}
+	int _Depth(GeneralListNode* head)
+	{
+		int depth = 1;
 
-	void Insert(GeneralListNode* pos, char value)
-	{}
+		GeneralListNode* begin = head;
+		while (begin)
+		{
+			if(begin->_type == SUB_TYPE)
+			{
+				int ret = _Depth(begin->_subLink);
+				if (depth < ret + 1)
+				{
+					depth = ret + 1;
+				}
+			}
 
-	void Erase(GeneralListNode* n)
-	{}
+			begin = begin->_next;
+		}
 
-	int Depth()
-	{}
+		return depth;
+	}
 
-	int Size()
-	{}
+	int _Size(GeneralListNode* head)
+	{
+		int size = 0;
+		GeneralListNode* begin = head;
+		while (begin)
+		{
+			if(begin->_type == SUB_TYPE)
+			{	
+				size += _Size(begin->_subLink);
+			}
+			else if (begin->_type == VALUE_TYPE)
+			{
+				++size;
+			}
+
+			begin = begin->_next;
+		}
+
+		return size;
+	}
+
+	void _Destroy(GeneralListNode* head)
+	{
+		GeneralListNode* begin = head;
+		while (begin)
+		{
+			GeneralListNode* del = begin;
+			begin = begin->_next;
+
+			if(del->_type == SUB_TYPE)
+			{
+				_Destroy(del->_subLink);
+			}
+
+			delete del;
+		}
+	}
+
+	GeneralListNode* _Copy(GeneralListNode* head) const
+	{
+		GeneralListNode* newHead = new GeneralListNode(HEAD_TYPE);
+
+		GeneralListNode* prev = newHead;
+		GeneralListNode* begin = head->_next;
+		while (begin)
+		{
+			GeneralListNode* cur = new GeneralListNode();
+
+			if(begin->_type == HEAD_TYPE)
+			{
+				cur->_type = HEAD_TYPE;
+			}
+			else if (begin->_type == SUB_TYPE)
+			{
+				cur->_type = SUB_TYPE;
+				cur->_subLink = _Copy(begin->_subLink);
+			}
+			else
+			{
+				cur->_type = VALUE_TYPE;
+				cur->_value = begin->_value;
+			}
+
+			prev->_next = cur;
+			prev = cur;
+
+			begin = begin->_next;
+		}
+
+		return newHead;
+	}
 
 private:
 	GeneralListNode* _head;
 };
 
+//<1> A = ()
+//<2> B = (a,b)
+//<3> C = (a,b,(c,d))
+//<4> D = (a,b,(c,d),(e,(f),h)) 
+
 void Test1()
 {
 	// 一层
-	//GeneralList gl("(a,b)");
-	//gl.Print();
+	GeneralList gl("(a,b)");
+	gl.Print();
 
-	//// 两层
-	//GeneralList g2("(a,b,(c,d),e)");
-	//g2.Print();
+	cout<<"Depth:"<<gl.Depth()<<endl;
+
+	// 两层
+	GeneralList g2("(a,b,(c,d))");
+	g2.Print();
+
+	cout<<"Depth:"<<g2.Depth()<<endl;
 
 	// 三层
-	//GeneralList g3("(a,b,(c,d),(f,(g),()))");
-	//g3.Print();
+	GeneralList g3("(a,b,(c,d),(e,(f),h))");
+	g3.Print();
+
+	cout<<"Depth:"<<g3.Depth()<<endl;
+	cout<<"Size:"<<g3.Size()<<endl;
 
 	// 多层空表嵌套
-	//GeneralList g4("(((),()))");
-	//g4.Print();
+	GeneralList g4("(((),()))");
+	g4.Print();
+
+	cout<<"Depth:"<<g4.Depth()<<endl;
+
+	GeneralList g5(g3);
+	g5.Print();
+
+	GeneralList g6 = g5;
+	g6.Print();
 }
